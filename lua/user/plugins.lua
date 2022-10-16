0a3ac9366bf196804cdb0b27915568753343c3d1
Joel <joel.salas@4geeks.io>
2022-10-15 19:25:46 -0600

    omnisharpConfig

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,6 +70,8 @@ return packer.startup(function(use)
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  
+  use 'OmniSharp/omnisharp-vim'
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
